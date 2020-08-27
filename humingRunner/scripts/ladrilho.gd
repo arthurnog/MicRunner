371f837d822cf23afa7e34c552fbc59cd081d0c1
arthurnog <artuts874@gmail.com>
2020-08-27 09:35:02 -0300

    minor changes

--- humingRunner/scripts/ladrilho.gd
+++ humingRunner/scripts/ladrilho.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var game
 var speed = 250
+var flag = 1
 
 func _ready():
 	game = get_tree().current_scene
@@ -15,9 +16,7 @@ func _process(delta):
 		if position.x < -100:
 			queue_free()
 
-
-	
-
-
 func _on_portal_area_entered(area):
-	game.countScore()
+	if flag == 1:
+		game.countScore()
+		flag = 0
